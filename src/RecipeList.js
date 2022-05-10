82ce17075dce321ea86844574659f96956dd4124
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-10 12:39:33 +0100

    Added recipe button to list

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -4,10 +4,13 @@ import Recipe from "./Recipe";
 // Recipe list functional component
 export default function RecipeList({ recipes }) {
   return (
-    <div>
-      {recipes.map((recipe) => {
-        return <Recipe key={recipe.id} {...recipe} />;
-      })}
-    </div>
+    <>
+      <div>
+        {recipes.map((recipe) => {
+          return <Recipe key={recipe.id} {...recipe} />;
+        })}
+      </div>
+      <button>Add Recipe</button>
+    </>
   );
 }
