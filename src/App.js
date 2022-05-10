98c2f8c7350bc5ec2e22dd8110c38cdb5380bb3a
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-10 11:50:28 +0100

    removed previous code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import React, { useState } from "react";
-import Counter from "./Counter";
-import CounterHooks from "./CounterHooks";
-
-// Provider
-export const ThemeContext = React.createContext();
+import React from "react";
 
 // Consumer
 function App() {
-  // State for setting color
-  const [theme, setTheme] = useState("red");
-  return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      Counter
-      <Counter initialCount={0} />
-      Counter Hooks
-      <CounterHooks initialCount={0} />
-      <button
-        onClick={() =>
-          setTheme((prevTheme) => {
-            return prevTheme === "red" ? "blue" : "red";
-          })
-        }
-      >
-        Toggle Theme
-      </button>
-    </ThemeContext.Provider>
-  );
+  return null;
 }
 
 export default App;
